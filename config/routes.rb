c5ce91b10eb3fd08aceb09150561accd3ca9eec2
sin0713 <narisin1124@gmail.com>
2021-12-05 10:17:39 +0000

    [Add]routs.rbに記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'homes#top'
+  get '/about' => 'homes#about'
+  
+  resources :users, only: [:edit, :show, :update] do
+    resource :relationships, only: [:create, :destroy]
+    member do
+      get :followings
+      get :followers
+    end 
+  end
+  
+  resources :recipes, only: [:new, :create, :edit, :update, :show, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+    get '/search' => 'recipes#search'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
