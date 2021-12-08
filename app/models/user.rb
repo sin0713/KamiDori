a4e08f0282c1c49b1b962b1609113c297789faa9
sin0713 <narisin1124@gmail.com>
2021-12-08 15:01:09 +0000

    [Add]バリデーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :recipes, dependent: :destroy
   has_many :recipe_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
   has_many :relationships, foreign_key: :follow_id
   has_many :followings, through: :relationships, source: :followed
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :followed_id
   has_many :followers, through: :reverse_of_relationships, source: :follow
-  
+
+  validates :name, presence: true
+
   attachment :image
-  
-  
+
+
   def is_followed_by?(user)
     reverse_of_relationships.find_by(follow_id: user.id).present?
-  end 
+  end
 end
