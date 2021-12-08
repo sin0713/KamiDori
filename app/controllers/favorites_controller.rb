06fd488ddea525ebd9d8e612dec34e8e0a5b7741
sin0713 <narisin1124@gmail.com>
2021-12-08 13:18:25 +0000

    [Add]良いね機能の非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -8,13 +8,11 @@ class FavoritesController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
     favorite = current_user.favorites.new(recipe_id: @recipe.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
      @recipe = Recipe.find(params[:recipe_id])
      favorite = current_user.favorites.find_by(recipe_id: @recipe.id)
      favorite.destroy
-     redirect_to request.referer
   end
 end
