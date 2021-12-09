e1bc320ce405ed38af382b5646936d5bce6d7517
sin0713 <narisin1124@gmail.com>
2021-12-09 06:45:14 +0000

    [Add]フォローボタンの非同期通信

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,15 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
+    @user = User.find(params[:user_id])
     following = current_user.relationships.build(followed_id: params[:user_id])
     following.save
-    redirect_to request.referer
   end
 
   def destroy
+    @user = User.find(params[:user_id])
     following = current_user.relationships.find_by(followed_id: params[:user_id])
     following.destroy
-    redirect_to request.referer
   end
 end
