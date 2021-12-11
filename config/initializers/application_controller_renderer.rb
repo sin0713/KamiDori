1331c03feb18bd9427d86d35860e63c332c9e9fa
sin0713 <narisin1124@gmail.com>
2021-12-11 14:28:56 +0000

    [Add]refileの設定

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -7,4 +7,4 @@
 #   )
 # end
 
-Refile.secret_key = ENV['REFILE_SECRET_KEY']
+Refile.secret_key = '043eacecec44779b15161486f2018d7ce486c2253ed6464d22e21ebb125560e66e558c2728b77fd6c8dbe67256393fff712bb0d38fe5780661e1b02a980ceccf'
