ad8dbfddae40d0a9b3ee473de158fb3801b499e6
sin0713 <narisin1124@gmail.com>
2021-12-14 17:08:02 +0000

    [Add]お気に入り一覧画面のテスト、レシピ編集画面のテスト、レシピ新規作成画面のテスト

--- spec/system/after_sign_in_spec.rb
+++ spec/system/after_sign_in_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe "Recipe", type: :system do
     let!(:taist) { create(:taist, recipe: recipe) }
     let!(:other_taist) { create(:taist, recipe: other_recipe) }
     let!(:recipe_comment) { create(:recipe_comment, recipe: recipe, user: user) }
+    let!(:favorite) { create(:favorite, recipe: other_recipe, user: user) }
     let!(:tools) { ["ハリオ", "カリタ", "メリタ"] }
     let!(:beans) { ["モカ", "キリマンジャロ", "コロンビア", "コナ", "マンデリン", "グアテマラ", "ブラジル", "ケニア"] }
 
@@ -210,7 +211,7 @@ RSpec.describe "Recipe", type: :system do
       before do
         visit recipe_path(other_recipe)
       end
-  
+
       context '表示の確認' do
         it 'URLが正しい' do
           expect(current_path).to eq ("/recipes/#{other_recipe.id}")
@@ -244,10 +245,144 @@ RSpec.describe "Recipe", type: :system do
            link = find('.profile__follow-btn')
            expect(link[:href]).to eq user_relationships_path(other_user)
         end
-      end 
+      end
     end
-    
+
     describe 'レシピ編集画面のテスト' do
-    end 
+      before do
+        visit edit_recipe_path(recipe)
+      end
+
+      context '表示の確認' do
+        it 'URLが正しい' do
+          expect(current_path).to eq '/recipes/' + recipe.id.to_s + '/edit'
+        end
+        it '「Edit」と表示される' do
+          expect(page).to have_content 'Edit'
+        end
+        it 'レシピテーブルのフォームがすべて表示されてる' do
+          expect(page).to have_field 'recipe[bean]', with: recipe.bean
+          expect(page).to have_field 'recipe[roast]', with: recipe.roast
+          expect(page).to have_field 'recipe[extraction_time_minutes]', with: recipe.extraction_time_minutes
+          expect(page).to have_field 'recipe[extraction_time_seconds]', with: recipe.extraction_time_seconds
+          expect(page).to have_field 'recipe[pre_infusion_time]', with: recipe.pre_infusion_time
+          expect(page).to have_field 'recipe[temperature]', with: recipe.temperature
+          expect(page).to have_field 'recipe[grind_size]', with: recipe.grind_size
+          expect(page).to have_field 'recipe[amount_of_beans]', with: recipe.amount_of_beans
+          expect(page).to have_field 'recipe[amount_of_extraction]', with: recipe.amount_of_extraction
+          expect(page).to have_field 'recipe[introduction]', with: recipe.introduction
+          expect(page).to have_field 'recipe[image]'
+        end
+        it 'テイストテーブルのフォームが表示されてるか' do
+          expect(page).to have_select '酸味'
+          expect(page).to have_select '苦味'
+          expect(page).to have_select '甘味'
+          expect(page).to have_select 'コク'
+          expect(page).to have_select '香り'
+        end
+        it 'Updateボタンが表示されてるか' do
+          expect(page).to have_button 'Update Recipe'
+        end
+      end
+
+      context '編集成功のテスト' do
+        before do
+          @recipe_old_bean = recipe.bean
+          @recipe_old_temperature = recipe.temperature
+          @taist_old_sour = taist.sour
+          fill_in 'recipe[bean]', with: Faker::Lorem.characters(number: 4)
+          fill_in 'recipe[temperature]', with: 100
+          select 5, from: '酸味'
+          click_button 'Update'
+        end
+
+        it '産地が正しく更新される' do
+          expect(recipe.reload.bean).not_to eq @recipe_old_bean
+        end
+        it '湯温が正しく更新される' do
+          expect(recipe.reload.temperature).not_to eq @recipe_old_temperature
+        end
+        it '酸味が正しく更新される' do
+          expect(taist.reload.sour).not_to eq @taist_old_sour
+        end
+        it 'リダイレクト先が、更新したレシピの詳細画面になっている' do
+          expect(current_path).to eq recipe_path(recipe)
+          expect(page).to have_content 'レシピを更新しました'
+        end
+      end
+    end
+
+    describe 'レシピ新規投稿画面のテスト(フォームの表示は編集画面のテストにて確認済み)' do
+      before do
+        visit new_recipe_path
+      end
+
+      context '表示の確認' do
+        it 'URLが正しいか' do
+          expect(current_path).to eq ("/recipes/new")
+        end
+        it 'createボタンが表示されているか' do
+          expect(page).to have_button 'Create Recipe'
+        end
+        it 'フォームに値が入ってない' do
+          expect(find_field('recipe[bean]').text).to be_blank
+          expect(find_field('recipe[tool]').text).to be_blank
+          expect(find_field('recipe[extraction_time_minutes]').text).to be_blank
+          expect(find_field('recipe[extraction_time_seconds]').text).to be_blank
+          expect(find_field('recipe[pre_infusion_time]').text).to be_blank
+          expect(find_field('recipe[temperature]').text).to be_blank
+          expect(find_field('recipe[amount_of_beans]').text).to be_blank
+          expect(find_field('recipe[amount_of_extraction]').text).to be_blank
+          expect(find_field('recipe[introduction]').text).to be_blank
+          expect(find_field('recipe[image]').text).to be_blank
+        end
+
+      end
+
+      context '投稿成功のテスト' do
+        before do
+          fill_in 'recipe[bean]', with: Faker::Lorem.characters(number: 5)
+          fill_in 'recipe[tool]', with: Faker::Lorem.characters(number: 5)
+          fill_in 'recipe[extraction_time_minutes]', with: 2
+          fill_in 'recipe[extraction_time_seconds]', with: 30
+          fill_in 'recipe[pre_infusion_time]', with: 30
+          fill_in 'recipe[temperature]', with: 90
+          fill_in 'recipe[amount_of_beans]', with: 50
+          fill_in 'recipe[amount_of_extraction]', with: 600
+          fill_in 'recipe[introduction]', with: Faker::Lorem.characters(number: 20)
+          select 'ライトロースト', from: '焙煎度'
+          select '細挽き', from: '挽目'
+          select 5, from: '酸味'
+          select 5, from: '苦味'
+          select 5, from: '甘味'
+          select 5, from: '香り'
+          select 5, from: 'コク'
+          find('.new-recipe__user-id', visible: false).set(1)
+          find('.new-recipe__recipe-id', visible: false).set(2)
+        end
+        it 'レシピが正しく保存されているか' do
+          expect { click_button 'Create Recipe'}.to change(user.recipes, :count).by(1)
+        end
+        it 'リダイレクト先が正しいか' do
+          click_button 'Create Recipe'
+          expect(current_path).to eq recipe_path(Recipe.last)
+          expect(page).to have_content '新しいレシピを投稿しました'
+        end
+      end
+    end
+
+    describe 'お気に入りレシピ一覧画面のテスト' do
+      before do
+        visit favorites_recipes_path
+      end
+
+      it 'URLが正しい' do
+        expect(current_path).to eq ("/recipes/favorites")
+      end
+      it 'お気に入りのレシピが表示されているか' do
+        @favorite_count = user.favorites.count
+        expect(all('.recipe-contents__items').count).to eq @favorite_count
+      end
+    end
   end
 end
